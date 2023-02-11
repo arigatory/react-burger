39bbaab0023b6aec33599642970e23e5270d2f27
Ivan MSI <arigatory@gmail.com>
2023-02-12 01:04:17 +0300

    test

--- src/app/redux/accountSlice.test.tsx
+++ src/app/redux/accountSlice.test.tsx
@@ -4,6 +4,6 @@ const reducer = accountSlice.reducer;
 
 test('should return the initial state', () => {
   expect(reducer(undefined, { type: undefined })).toEqual([
-    { text: 'Use Redux', completed: false, id: 0 }
+    { profile: null, status: 'idle' }
   ])
 })
